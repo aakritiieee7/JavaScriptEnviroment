7af6be87b8471b95ea6f43e900dac538c12f0513
aakritiieee7 <reachaakriti7@gmail.com>
2025-07-25 01:30:23 +0000

    datatypes

---
+++ basics/datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all JS code as newer version since there exists an old one too
+
+// alert( 3 + 3) // we are using node.js, hence cannot generate alerts right now, if using browser we could by inspect -> console
+
+// can use ; for readibility however not necessary syntactically
+
+let name = "aakriti"
+let age = 18
+let isLoggedIn = true
+let state;
+
+// number => range 2 to power 53
+// bigint larger number
+// string => "" or ''
+// boolean => true/false
+// null => standalone value signifying no presence of variable's quantifiability 
+// undefined => signies no definition of variable
+// symbol => to define UNIQUE elements like in a react frontend or say a figma design 
+
+
+console.log(typeof givenvariable); // returns type of given variable
+
+console.log(typeof name); // string
+console.log(typeof undefined); // undefined
+console.log(typeof null); // interestingly, null is an object according to js but a data type according to most other standards
