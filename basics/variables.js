b3ab52525f12b2ccbfa6dca407c66f086a3823dc
aakritiieee7 <reachaakriti7@gmail.com>
2025-07-25 01:18:58 +0000

    varibles

---
+++ basics/variables.js
@@ -0,0 +1,9 @@
+const id = "1234" //constant value does not accept assignment
+let name = "aakriti" //functional and block scope are well defined, replaces var use
+var number = 100 // does not have any scope defined hence advised not to be used
+city = "delhi"; //allows no type assigned variables too
+let accountid; //undefined
+
+console.log("id");
+
+console.table([name,id,city,number,accountid]); //prints an index(0 to n-1) vs values table
